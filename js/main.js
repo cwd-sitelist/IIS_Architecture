53c9186b687e9e6e870c5b5261a03ace2729cd58
sujithcwd-eng <sujithcwd@gmail.com>
2026-05-28 18:20:55 +0530

    code cleeaning and reesponsive

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+        const serviceSelect = document.getElementById("serviceSelect");
+        const rateInput = document.getElementById("rate");
+        const costInput = document.getElementById("cost");
+
+        serviceSelect.addEventListener("change", function () {
+
+            const value = this.value;
+
+            if (value) {
+
+                const data = value.split("|");
+
+                rateInput.value = "£" + data[0];
+                costInput.value = "£" + data[1];
+
+            } else {
+
+                rateInput.value = "";
+                costInput.value = "";
+            }
+
+        });
+
+
+         const openPopup = document.getElementById("openPopup");
+        const closePopup = document.getElementById("closePopup");
+        const popupForm = document.getElementById("popupForm");
+
+        openPopup.addEventListener("click", () => {
+            popupForm.classList.add("active");
+        });
+
+        closePopup.addEventListener("click", () => {
+            popupForm.classList.remove("active");
+        });
+
+        window.addEventListener("click", (e) => {
+            if (e.target === popupForm) {
+                popupForm.classList.remove("active");
+            }
+        });
+
+
